c93cceff72677598a53e0f6c401fa7732fefe063
rovedabr <rovedabr@gmail.com>
2023-05-17 11:49:35 -0300

    NLW12.Mobile aula2

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
   return (
-    <View className='bg-gray-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 text-5xl'>Este é um Teste</Text>
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 text-8xl">Te Amo</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-
