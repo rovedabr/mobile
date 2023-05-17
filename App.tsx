dcf69948b04c7074e1e532623ed606f8da6dbec2
rovedabr <rovedabr@gmail.com>
2023-05-17 12:43:25 -0300

    Styles ok start page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, ImageBackground } from 'react-native'
+import { Text, ImageBackground, View, TouchableOpacity } from 'react-native'
+import { styled } from 'nativewind'
 
 import {
   useFonts,
@@ -10,6 +11,11 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/logo.svg'
+
+const StyledStripes = styled(Stripes)
+const StyledLogo = styled(Logo)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -25,10 +31,35 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="bg-gray-900 flex-1 items-center relative"
-      imageStyle={{position: 'absolute', left: '-100%'}}
+      className="bg-gray-900 flex-1 items-center relative px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
     >
-      <Text className="text-gray-50 text-5xl font-alt font-bold">Te Amo</Text>
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <StyledLogo />
+        <View className="space-y-2">
+          <Text className="text-gray-50 text-center font-title text-2xl leading-tight">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-gray-100 font-body text-center text-base leading-relaxed">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-base leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="auto" />
     </ImageBackground>
   )
