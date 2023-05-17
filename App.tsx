9447be66f3109576f4c8e91fcafaa40f7618e245
rovedabr <rovedabr@gmail.com>
2023-05-17 12:15:50 -0300

    Styles start page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, ImageBackground } from 'react-native'
 
 import {
   useFonts,
@@ -9,11 +9,27 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
+import blurBg from './src/assets/bg-blur.png'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 text-8xl">Te Amo</Text>
+    <ImageBackground
+      source={blurBg}
+      className="bg-gray-900 flex-1 items-center relative"
+      imageStyle={{position: 'absolute', left: '-100%'}}
+    >
+      <Text className="text-gray-50 text-5xl font-alt font-bold">Te Amo</Text>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   )
 }
