29e4b8f6ee54b41a790d3dd0d6c77b9bed66ade0
rovedabr <rovedabr@gmail.com>
2023-05-19 09:34:28 -0300

    criação das páginas

---
+++ app/index.tsx
@@ -0,0 +1,114 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, ImageBackground, View, TouchableOpacity } from 'react-native'
+import { styled } from 'nativewind'
+import * as SecureStore from 'expo-secure-store'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/logo.svg'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { discovery } from 'expo-auth-session/build/providers/Facebook'
+import { useEffect } from 'react'
+import { api } from './src/lib/api'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/5254107957705fe1bc11',
+}
+
+const StyledStripes = styled(Stripes)
+const StyledLogo = styled(Logo)
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  const [request, response, singInWithGitHub] = useAuthRequest(
+    {
+      clientId: '5254107957705fe1bc11',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = request.extraParams
+      api
+        .post('/register', {
+          code,
+        })
+        .then((response) => {
+          const { token } = response.data
+
+          SecureStore.setItemAsync('token', token)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }, [response])
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <StyledLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => singInWithGitHub}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-base leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="auto" />
+    </ImageBackground>
+  )
+}
